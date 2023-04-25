50b5ec8b5bd29e6eef572d5784679f11ca74325b
vperizes <vperizes@gmail.com>
2023-04-25 13:35:21 -0500

    Added python scripts that practice plotting functions and points.

---
+++ PlotPoints.py
@@ -0,0 +1,48 @@
+import math
+import numpy as np
+
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('Graphs in PyOpenGL')
+
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 4, -1, 1)
+
+
+def plot_graph():
+    glBegin(GL_POINTS)
+    px: GL_DOUBLE  # px = point x
+    py: GL_DOUBLE
+    for px in np.arange(0, 4, 0.005):
+        py = math.exp(-px) * math.cos(2* math.pi * px)
+        glVertex2f(px, py)
+    glEnd()
+
+
+done = False
+init_ortho()
+glPointSize(5)
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    plot_graph()
+    pygame.display.flip()
+    pygame.time.wait(100)
+pygame.quit()
